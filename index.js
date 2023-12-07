612f49c6319b4723410c36dc2553767194a2d63f
NiravRathva <niravrtw01@gmail.com>
2023-12-07 17:00:28 +0530

    setup a express app and connted to database

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello World!")
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const app = express();
+const port = 8000;
+dotenv.config();
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO).then(() => {
+      console.log("Connnected To MongoDb");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+app.listen(port, () => {
+  console.log(` app listening on port ${port}`);
+  connectToMongo();
+});
