98c2e94ecb838b1875d84c6c3502f05e18da294f
NiravRathva <niravrtw01@gmail.com>
2023-12-11 10:43:04 +0530

    added catchAsync util function and signin functionality

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,23 +1,38 @@
 import User from "../model/UserModel.js";
 import jwt from "jsonwebtoken";
-
+import { catchAsync } from "../utils/catchAsync.js";
+import { appError } from "../utils/appError.js";
 //function for signing token
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET);
 };
 
-export const signUp = async (req, res,next) => {
-  try {
-    const user = await User.create({
-      email: req.body.email,
-      password: req.body.password,
-    });
-    //signing token
-    const token = signToken(user._id);
-    //sending response
-    res.status(201).json({ user, token });
-  } catch (error) {
-    next(error)
+export const signUp = catchAsync(async (req, res, next) => {
+  const user = await User.create({
+    email: req.body.email,
+    password: req.body.password,
+  });
+  //signing token
+  const token = signToken(user._id);
+  //sending response
+  res.status(201).json({ user, token });
+});
+export const signIn = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  //check if email  and password if exist
+  if (!email || !password) {
+    return next(new appError("please provide email  and password", 400));
   }
-};
-export const signIn = async () => {};
+
+  // check if the user exits
+  const user = await User.findOne({ email: email }).select("+password");
+
+  if (!user || !(await user.verifyPassword(password, user.password))) {
+    return next(new appError("Wrong credentials", 401));
+  }
+
+  const token = signToken(user._id);
+  user.password = undefined;
+  //sending response
+  res.status(200).json({ user, token });
+});
